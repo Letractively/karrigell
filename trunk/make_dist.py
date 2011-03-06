383c9a34f98474620522d322811f366ff654c206
pierre.quentel@gmail.com <pierre.quentel@gmail.com@218455f0-624d-b2e9-1d42-c971c4d590c7>
2011-03-06 20:42:33 +0000

    Update make_dist

--- trunk/make_dist.py
+++ trunk/make_dist.py
@@ -3,7 +3,8 @@ import tarfile
 import Karrigell
 
 name = 'Karrigell-{}'.format(Karrigell.version)
-dist = tarfile.open(os.path.join('dist',name+'.gz'),mode='w:gz')
+dist_dir = os.path.join(os.path.dirname(os.getcwd()),'dist')
+dist = tarfile.open(os.path.join(dist_dir,name+'.gz'),mode='w:gz')
 
 for path in ['README.txt','MANIFEST','setup.py']:
     dist.add(path,arcname=os.path.join(name,path))
@@ -19,14 +20,14 @@ for path in ['Karrigell','HTMLTags']:
                 arcname=os.path.join(name,path,filename))
 
 # admin tools
-for (dirpath,dirnames,filenames) in os.walk(os.path.join(os.path.dirname(os.getcwd()),'admin_tools')):
-    exclude = [ d for d in dirnames if d[0] in '._' ]
-    for d in exclude:
-        dirnames.remove(d)
-    for filename in [ f for f in filenames 
-        if not os.path.splitext(f)[1] in ['.sqlite'] ]:
-        print('add',filename)
-        dist.add(os.path.join(dirpath,filename),
-            arcname=os.path.join(name,'admin_tools',path,filename))
+tools_folder = os.path.join(os.path.dirname(os.getcwd()),'admin_tools')
+for path in os.listdir(tools_folder):
+    if not os.path.isfile(os.path.join(tools_folder,path)):
+        continue
+    if os.path.splitext(path)[1] in ['.sqlite']:
+        continue
+    print('add',path)
+    dist.add(os.path.join(tools_folder,path),
+        arcname=os.path.join(name,'admin_tools',path))
 
 dist.close()
