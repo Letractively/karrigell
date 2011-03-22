32081663a016d5dd4146c1cec04d1f7f4995a952
pierre.quentel@gmail.com <pierre.quentel@gmail.com@218455f0-624d-b2e9-1d42-c971c4d590c7>
2011-03-22 15:39:53 +0000

    In default server, use an application class, not an instance

--- www/server.py
+++ www/server.py
@@ -3,15 +3,15 @@ import Karrigell
 
 # create an application
 # by default, root url is / and root directory is this directory
-app = Karrigell.App()
+class MyApp(Karrigell.App):
 
-# set users database
-users_path = os.path.join(os.path.dirname(os.getcwd()),'data','users.sqlite')
-app.users_db = Karrigell.admin_db.SQLiteUsersDb(users_path)
+    # set users database
+    users_path = os.path.join(os.path.dirname(os.getcwd()),'data','users.sqlite')
+    users_db = Karrigell.admin_db.SQLiteUsersDb(users_path)
 
-# set translation database
-transl_path = os.path.join(os.path.dirname(os.getcwd()),'data','translation.sqlite')
-app.translation_db = Karrigell.admin_db.SQLiteTranslationDb(transl_path)
+    # set translation database
+    transl_path = os.path.join(os.path.dirname(os.getcwd()),'data','translation.sqlite')
+    translation_db = Karrigell.admin_db.SQLiteTranslationDb(transl_path)
 
 # start the web server on port 80 to serve this application
-Karrigell.run(apps=[app])
+Karrigell.run(apps=[MyApp])
