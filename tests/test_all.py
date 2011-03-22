4a637c75e57ae99405d3b33f8592d8f4d0fbe7cb
pierre.quentel@gmail.com <pierre.quentel@gmail.com@218455f0-624d-b2e9-1d42-c971c4d590c7>
2011-03-22 14:11:38 +0000

    More tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,4 @@
 import sys
-k_path = r'c:\Karrigell-Python3\20110223_2'
-sys.path.insert(0,k_path)
 
 import Karrigell
 import threading
@@ -21,20 +19,27 @@ Hello tartampion
 
 class TestServer(threading.Thread):
 
-    def run(self):
-        Karrigell.run(port=8082)
+    def __init__(self,**kw):
+        self.kw = kw
+        threading.Thread.__init__(self)
 
-server = TestServer()
-server.start()
+    def run(self,**kw):
+        Karrigell.run(**self.kw)
 
 class Tester(unittest.TestCase):
 
+    def test_not_found(self):
+        with self.assertRaises(urllib.error.HTTPError) as cm:
+            res = urllib.request.urlopen(self.start+"/xyz.azert")
+        the_exception = cm.exception
+        self.assertEqual(the_exception.code, 404)
+
     def test_echo(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/hello?name=tartampion")
+        res = urllib.request.urlopen(self.start+"/test.py/hello?name=tartampion")
         self.assertEqual(res.read(),b'Hello tartampion')
 
     def test_template(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/bar?name=tartampion")
+        res = urllib.request.urlopen(self.start+"/test.py/bar?name=tartampion")
         info = res.info()
         self.assertIn('Set-Cookie',info)
         cookie = http.cookies.BaseCookie(info['Set-Cookie'])
@@ -43,42 +48,77 @@ class Tester(unittest.TestCase):
         self.assertEqual(res.read(),template.encode('ascii'))
 
     def test_import(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/import_test")
+        res = urllib.request.urlopen(self.start+"/test.py/import_test")
         self.assertEqual(res.read(),b'<B>foobar</B>')
 
     def test_file_session(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/set_session?name=shelley")
+        res = urllib.request.urlopen(self.start+"/test.py/set_session?name=shelley")
         info = res.info()
         self.assertIn('set-cookie',info)
         cookie = info['set-cookie'].split('=')
         self.assertEqual(cookie[0],"session_id")
         session_id = cookie[1]
-        req = urllib.request.Request("http://localhost:8082/test.py/get_session")
+        req = urllib.request.Request(self.start+"/test.py/get_session")
         req.add_header('Cookie',"=".join(cookie))
         res = urllib.request.urlopen(req)
         self.assertEqual(res.read(),b'shelley')
 
     def test_memory_session(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/set_session?name=shelley")
+        res = urllib.request.urlopen(self.start+"/test.py/set_session?name=shelley")
         info = res.info()
         self.assertIn('set-cookie',info)
         cookie = info['set-cookie'].split('=')
         self.assertEqual(cookie[0],"session_id")
         session_id = cookie[1]
-        req = urllib.request.Request("http://localhost:8082/test.py/get_session")
+        req = urllib.request.Request(self.start+"/test.py/get_session")
         req.add_header('Cookie',"=".join(cookie))
         res = urllib.request.urlopen(req)
         self.assertEqual(res.read(),b'shelley')
 
     def test_cookie(self):
-        res = urllib.request.urlopen("http://localhost:8082/test.py/set_cookie?name=band&val=smiths")
+        res = urllib.request.urlopen(self.start+"/test.py/set_cookie?name=band&val=smiths")
         info = res.info()
         self.assertIn('set-cookie',info)
         cookie = info['set-cookie'].split('=')
         self.assertEqual(cookie[0],"band")
-        req = urllib.request.Request("http://localhost:8082/test.py/read_cookie?name=band")
+        req = urllib.request.Request(self.start+"/test.py/read_cookie?name=band")
         req.add_header('Cookie',"=".join(cookie))
         res = urllib.request.urlopen(req)
         self.assertEqual(res.read(),b'smiths')
 
-unittest.main()
+# test with default app
+server = TestServer(port=8082)
+server.start()
+
+# test with extra app
+class App1(Karrigell.App):
+
+    def hide_ext(handler):
+        if handler.path_info.endswith('.sqlite'):
+            print('raise exception')
+            raise Karrigell.HTTP_ERROR(403)
+
+    filters = [hide_ext]
+
+server = TestServer(apps=[App1],port=8083)
+server.start()
+
+class Tester1(Tester):
+    start = "http://localhost:8082"
+
+class Tester2(Tester):
+    start = "http://localhost:8083"
+
+    def test_filter(self):
+        # check that files with extension sqlite are forbidden
+        with self.assertRaises(urllib.error.HTTPError) as cm:
+            res = urllib.request.urlopen(self.start+"/truc.sqlite")
+        the_exception = cm.exception
+        self.assertEqual(the_exception.code, 403)
+
+suite = unittest.TestSuite()
+suite1 = unittest.TestLoader().loadTestsFromTestCase(Tester1)
+suite2 = unittest.TestLoader().loadTestsFromTestCase(Tester2)
+
+suite.addTests([suite1,suite2])
+unittest.TextTestRunner(verbosity=1).run(suite)
