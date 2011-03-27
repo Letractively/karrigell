4ab1f5dd01d419ce46345dfa1c93e00057df0cd2
pierre.quentel <pierre.quentel@gmail.com>
2011-03-27 09:37:47 +0000

    More checking and explicit error messages in check_apps.py and admin_db.py

--- Karrigell/check_apps.py
+++ Karrigell/check_apps.py
@@ -1,7 +1,14 @@
+import os
+
 class ConfigError(Exception):
     pass
 
 def check(apps):
     for app in apps:
+        # root_url start with /
         if not app.root_url.startswith('/'):
             raise ConfigError('Root url {} does not start with "/"'.format(app.root_url))
+        # root dir exists
+        if not os.path.exists(app.root_dir):
+            raise ConfigError('Root directory {} does not exist'.format(app.root_dir))
+        
