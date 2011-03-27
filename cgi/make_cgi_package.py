a886fa98bc8e6133de3df8e524d8b82209a51935
pierre.quentel@gmail.com <pierre.quentel@gmail.com@218455f0-624d-b2e9-1d42-c971c4d590c7>
2011-03-27 09:36:54 +0000

    Adapt make_cgi_package.py because of bug 11272 : on Windows, input() doesn't strip the trailing CR

--- cgi/make_cgi_package.py
+++ cgi/make_cgi_package.py
@@ -16,6 +16,12 @@ os.mkdir(local_cgi_dir)
 local_root_dir = os.path.join(dest_dir,'root_directory')
 os.mkdir(local_root_dir)
 
+def _input(prompt=''):
+    # replaces built-in input() : in Python 3.2, 
+    # doesn't strip CR on Windows (bug #11272)
+    res = input(prompt)
+    return res.rstrip('\r\n')
+
 print(
 """Builds a Karrigell package to install in the CGI folder of a web server
 
@@ -29,7 +35,7 @@ It is usually something like "c:/Python3.x/python.exe" or simply "python" on
 Windows, and /usr/local/bin/python on Unix-like operating systems. If you are 
 not sure, ask the administrator or check the site documentation
 """)
-python_path = input('Python interpreter path : ')
+python_path = _input('Python interpreter path : ')
 
 print("""
 You must define at least 1 application, served by the "root url", for instance
@@ -38,7 +44,7 @@ application serves the requests at addresses like
 http://host/foo/script.py/func""")
 
 while True:
-    root_url = input('Root url (must begin with /) : ')
+    root_url = _input('Root url (must begin with /) : ')
     if root_url.startswith('/'):
         break
 
@@ -47,12 +53,13 @@ Specify the directory in the file system where the scripts for this root url
 are found""")
 
 while True:
-    root_dir = input('Root directory : ')
+    root_dir = _input('Root directory : ')
     if root_dir:
         break
 
 while True:
-    set_users_db = input("Do you want to set up a users database (Y/N) ? ")
+    set_users_db = _input("Do you want to set up a users database (Y/N) ? ")
+    set_users_db = set_users_db.strip() # bug in Python 3.2 : doesn't strip CR
     if set_users_db.lower() in "yn":
         break
 
@@ -61,7 +68,7 @@ if set_users_db.lower()=="y":
     "the path in the file system for this database. Make sure not to put it "
     "in the root directory defined above, for security reasons")
     while True:
-        users_db_path = input("Path of SQLite database in the file system : ")
+        users_db_path = _input("Path of SQLite database in the file system : ")
         if users_db_path:
             break
 
@@ -86,7 +93,7 @@ out.close()
 
 # create folder for document root with the .htaccess file and an index file
 while True:
-    cgi_url = input('\nEnter the url path of the CGI directory (usually /cgi-bin) :')
+    cgi_url = _input('\nEnter the url path of the CGI directory (usually /cgi-bin) :')
     if cgi_url.startswith('/'):
         break
 htaccess = """
