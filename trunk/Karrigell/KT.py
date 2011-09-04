b594889fc8cfc3554210d77df191919a222b017b
pierre.quentel@gmail.com <pierre.quentel@gmail.com@218455f0-624d-b2e9-1d42-c971c4d590c7>
2011-09-04 08:24:47 +0000

    New version of Karrigell Template module (by Jim Eggleston)

--- trunk/Karrigell/KT.py
+++ trunk/Karrigell/KT.py
@@ -1,45 +1,98 @@
 """KT -- Karrigell Template
 
 Provides a means of performing substitutions and translations in templates
-stored in separate files. Template files are referenced by urls, following the 
-same conventions as the Import statement. Templates can include other templates.
+stored in separate files. Template files are referenced by URLs when KT is 
+used within the Karrigell framework .Templates can include other templates.
 
-Template syntax:
-
+Template syntax
+===============
 @[template_url]
   include template referenced by template_url]
     
-#[string_to_translate]
+_[string_to_translate]
   apply standard Karrigell string translation to string_to_translate
     
 $identifier
   subtitute the value of identifier
+  
+$container.attribute
+  substitute the value of container.attribute or container['attribute'] 
     
 These combinations are also possible:
 
 @[$template_url]
-#[$string]
+_[$string]
 
-Usage:
-text = KT(template_url, id1=val1, id2=val2, ...)
-  or    
-text = KT(template_url, **data)
+Initialisation within a Karrigell script
+========================================
+KT is included as a keyword within Karrigell. Use it like this:
+
+kt = KT()
+
+kt can also be initialised with a default encoding and a translation
+function:
+
+ENCODING = 'utf-8'
+_ = Import('translations.py').translate
+kt = KT(translate=_, encoding=ENCODING)
+
+Rendering a template
+====================
+There are a number of possible ways to pass data to a template:
+
+1. Passing individual variables:
+text = kt.render(templ_file, id1=val1, id2=val2, ...)
+
+2. Passing a dictionary: 
+text = kt.render(templ_url, **data)
   where data is a dictionary of values to substitute.
+
+3. Passing dictionaries or objects by name:
+text = kt.render(templ_file, data=locals(), this=THIS)
+  Values/attributes of the passed object can be referenced 
+  using dot notation in the templates:
+  $data.var1   $this.root_dir
 
 Encoding management
 ===================
-The contents of the files managed by KT are supposed to be bytestrings
-encoded with the encoding defined by SET_UNICODE_OUT()
+The default encoding for the TemplateEngine instance can be specified on 
+initialisation:
 
-If they are encoded with another encoding, you can specify it with :
-    KT(template_url,encoding=foo)
+kt = KT(encoding='utf-8')
 
-All the included urls in this template are supposed to be also encoded with
+If a specific template is encoded with another encoding, you can specify it with :
+text = kt.render(template_file, encoding=foo)
+
+All the included urls in this template are also supposed to be encoded with
 this encoding. If some of them are encoded with a different encoding their
-encoding must be specified after the included url name :
-@[included_url,encoding]
+encoding must be specified after the included template file name :
+@[included_templ,encoding]
+
+Use of KT as a stand-alone template engine
+==========================================
+KT can also be used as a stand-alone template engine. When used outside of the
+Karrigell framework, file system paths are used instead of URLs to give the 
+locations of the templates. 
+
+from Karrigell.KT import KT
+kt = KT(encoding='utf-8')
+var1 = 'value 1'
+var2 = 'value 2'
+print(kt.render('master.kt', data=locals())
+
+For convenience, a template directory can be specified on initialisation. e.g.
+
+from Karrigell.KT import KT
+kt = KT(templ_dir='/myapp/templates', encoding='utf-8')
+var1 = 'value 1'
+var2 = 'value 2'
+print(kt.render('master.kt', data=locals())
+
+In this case 'master.kt' is joined to '/myapp/templates' using os.path.join to 
+give the full path to the template file.
 """
 
+import sys, os
 import urllib.parse
 import re
 
@@ -52,64 +105,88 @@ class NotFoundError(Exception):
 class KTError(Exception):
     pass
 
+"""
 def _log(*data):
     import sys
     for item in data:
         sys.stderr.write('%s\n' %item)
+"""
 
-class action:
-    def __init__(self, handler):
-        self.handler = handler
-        self.included_urls = set()
-    
-    def __call__(self,tmpl_url,encoding='utf-8',**namespace):
+class KT:
+    def __init__(self, templ_dir='',
+                encoding='utf-8',
+                translate=str,
+                handler=None
+                ):
+        self.translation_func = translate
         self.encoding = encoding
-        abs_url = self.handler.abs_url(tmpl_url)
-        fs_path = self.handler.get_file(abs_url)
-        with open(fs_path,encoding=encoding,errors='replace') as fileobj:
+        self.templ_dir = templ_dir
+        self.handler = handler
+        self.included_paths = []
+    
+    def __call__(self,template,encoding='utf-8',**namespace):
+        return self.render(template,encoding='utf-8',**namespace)
+        
+    def render(self,template,encoding=None,**namespace):
+        if not encoding:
+            encoding = self.encoding
+        # If handler is defined then we are running within the 
+        # Karrigell framework. Convert URLs to absolute fs paths
+        if self.handler:
+            abs_url = self.handler.abs_url(template)
+            templ_path = self.handler.get_file(abs_url)
+        else:
+            templ_path = os.path.join(self.templ_dir,template)
+        if not os.path.isfile(templ_path):
+            raise NotFoundError('No template file at path %s' %templ_path)
+        with open(templ_path,encoding=encoding,errors='replace') as fileobj:
             templ = fileobj.read()               
-        templ = self._include(templ, namespace, abs_url)
-        templ = self._substitute(templ, namespace)
-        data = self._translate(templ)
-        return data
+        self.included_paths.append(templ_path)
+        result = self._include(templ, namespace, templ_path)
+        result = self._substitute(result, namespace)
+        if self.translation_func:
+            result = self._translate(result)
+        return result
 
-    def _include(self, text, namespace, parent_url):
+    def _include(self, text, namespace, parent_path):
         regex = re.compile(r"""
             \@(?:
                 (?P<escaped>\@)                     | # Escape sequence of two delimiters
                 \[(?P<id>\$[_a-z][_a-z0-9\.]*)\]    | # a bracketed Python identifier
-                \[(?P<url>.*?[^\\])\]               | # a bracketed url
+                \[(?P<path>.*?[^\\])\]              | # a bracketed url
                 (?P<invalid>)                         # Other ill-formed delimiter exprs
             )
             """, re.IGNORECASE | re.VERBOSE)
         def helper(mo):
+            path = None
             if mo.group('escaped') is not None:
                 return mo.group(0)
             url = ''
             if mo.group('id') is not None:
                 id = str(mo.group('id'))
-                url = self._substitute(id, namespace)
+                path = self._substitute(id, namespace)
                 # if id has no value, don't attempt to open the url.
-                if not url or url==id:
+                if not path or path==id:
                     return ''
-            if mo.group('url') is not None:
-                url = str(mo.group('url'))
-            if url:
+            if mo.group('path') is not None:
+                path = str(mo.group('path'))
+            if path:
                 encoding = self.encoding
-                if ',' in url:
-                    url,encoding = url.split(',',1)
-                abs_url = urllib.parse.urljoin(parent_url, url)
-                if abs_url in self.included_urls:
-                    raise RecursionError("Circular reference to template %s in %s" % (abs_url, parent_url))
-                else:
-                    self.included_urls.add(abs_url)
-                try:
-                    fs_path = self.handler.get_file(abs_url)
-                except:
-                    raise NotFoundError('No file at url %s' %abs_url)
-                with open(fs_path,encoding=encoding,errors='replace') as fileobj:
+                if ',' in path:
+                    path,encoding = path.split(',',1)
+                parent_dir = os.path.dirname(parent_path)
+                abs_path = os.path.join(parent_dir, path)
+                self.included_paths.append(abs_path)
+                if not os.path.isfile(abs_path):
+                    raise NotFoundError('No template file at path %s' %abs_path)
+                with open(abs_path,encoding=encoding,errors='replace') as fileobj:
                     inclusion = fileobj.read()
-                return self._include(inclusion, namespace, abs_url)
+                try:
+                    return self._include(inclusion, namespace, abs_path)
+                except RuntimeError:
+                    raise RecursionError( \
+                        'Possible circular reference in template inclusions. Template inclusion list is:\n' + \
+                        '\n'.join(self.included_paths)) 
             if mo.group('invalid') is not None:
                 return mo.group(0)
         return regex.sub(helper, text)
@@ -132,19 +209,7 @@ class action:
                 except KeyError:
                     return delimiter + named
                 # make sure res is a string (8-bit or unicode) and not an instance. 
-                res = '%s' % res  
-                ### Not sure this kind of processing is required. Seems to works better
-                ### when commented out! 
-                # Convert to requested encoding if required 
-                #~ if self.encoding != self.handler.encoding:
-                    #~ encoded = res.encode(self.handler.encoding)
-                    #~ try:
-                        #~ res = encoded.decode(self.encoding)
-                    #~ except UnicodeDecodeError as msg:
-                        #~ msg = 'Warning: error trying to encode translation %s to %s' \
-                                #~ %(res,self.encoding)
-                        #~ print(msg)
-                return res            
+                return '%s' % res  
             container = mo.group('container')
             if container is not None:
                 if not container in namespace:
@@ -159,19 +224,7 @@ class action:
                     except:
                         res = mo.group(0)
                 # make sure res is a string (8-bit or unicode) and not an instance. 
-                res = '%s' % res
-                ### Not sure this kind of processing is required. Seems to works better
-                ### when commented out! 
-                #~ # Convert to requested encoding if required 
-                #~ if self.encoding != self.handler.encoding:
-                    #~ encoded = res.encode(self.handler.encoding)
-                    #~ try:
-                        #~ res = encoded.decode(self.encoding)
-                    #~ except UnicodeDecodeError as msg:
-                        #~ msg = 'Warning: error trying to encode translation %s to %s' \
-                                #~ %(res,self.encoding)
-                        #~ print(msg)
-                return res            
+                return '%s' % res
             if mo.group('escaped') is not None:
                 return delimiter
             if mo.group('invalid') is not None:
@@ -191,36 +244,7 @@ class action:
                 return mo.group(0)
             if mo.group('string') is not None:
                 val = str(mo.group('string')).replace('\]', ']')
-                res = self.handler.translation(val)
-                
-                ### Not sure this kind of processing is required. Seems to works better
-                ### when commented out! 
-                # If there was a translation then it is encoded with the handler's encoding.
-                # Try to convert it to the requested encoding. If conversion fails, print a 
-                # warning and pass the original value.
-                #~ if res != val and self.encoding != self.handler.encoding:
-                    #~ encoded = res.encode(self.handler.encoding)
-                    #~ try:
-                        #~ res = encoded.decode(self.encoding)
-                    #~ except UnicodeDecodeError as msg:
-                        #~ msg = 'Warning: error trying to encode translation %s to %s' \
-                                #~ %(res,self.encoding)
-                        #~ print(msg)
-                            
-                return res
+                return self.translation_func(val)
             if mo.group('invalid') is not None:
                 return mo.group(0)
         return regex.sub(helper, text)
-    
-    def get_strings(self, script):
-        regex = re.compile(r'_\[(.*?[^\\])\]')
-        f = open(script, 'r')
-        res = []
-        for line in f.readlines():
-            found = regex.findall(line)
-            for _string in found:
-                if _string not in res:
-                    res.append(_string)
-        f.close()
-        return res
-
